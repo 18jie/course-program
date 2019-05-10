62e4c53bbc9fe7186deb0cc23d0cc6d6f2c96d82
fengjie <fj@choicesoft.com.cn>
2019-05-10 17:59:21 +0800

    提交sql

--- src/main/resources/mybatis/sql/course_programr.sql
+++ src/main/resources/mybatis/sql/course_programr.sql
@@ -14,14 +14,46 @@ MySQL - 5.7.22-log : Database - course_program
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 USE `course_program`;
 
-/*Table structure for table `class` */
+/*Table structure for table `course_question` */
 
-DROP TABLE IF EXISTS `class`;
+DROP TABLE IF EXISTS `course_question`;
 
-CREATE TABLE `class` (
+CREATE TABLE `course_question` (
   `id` varchar(24) NOT NULL,
-  `course_id` varchar(24) NOT NULL COMMENT '指向课程',
-  `name` varchar(30) NOT NULL,
+  `course_id` varchar(24) NOT NULL,
+  `question_no` int(11) DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL COMMENT '题目类型。1-选择题；2-编程题',
+  `title` varchar(30) DEFAULT NULL,
+  `info` text,
+  `answer` varchar(10) DEFAULT NULL,
+  `system_in` text,
+  `system_out` text,
+  `create_time` date DEFAULT NULL,
+  `create_user` varchar(24) DEFAULT NULL,
+  `update_time` date DEFAULT NULL,
+  `update_user` varchar(24) DEFAULT NULL,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `course_question` */
+
+insert  into `course_question`(`id`,`course_id`,`question_no`,`level`,`type`,`title`,`info`,`answer`,`system_in`,`system_out`,`create_time`,`create_user`,`update_time`,`update_user`,`delete_flag`) values ('5cd3d6b4b94dab0c7cb43d8d','test1',2,1,1,'最小的数','<p>从下面的选项中选择最小的数：</p><p>A.1</p><p>B.2</p><p>C.3</p><p>D.4</p>','A',NULL,NULL,'2019-05-09','',NULL,NULL,0),('5cd3d777b94dab3984dc555f','test1',3,1,2,'输出Hello World！','<p>从控制台中输出Hello World!</p>',NULL,'',NULL,'2019-05-09','',NULL,NULL,0);
+
+/*Table structure for table `operation` */
+
+DROP TABLE IF EXISTS `operation`;
+
+CREATE TABLE `operation` (
+  `id` varchar(24) NOT NULL,
+  `course_id` varchar(24) NOT NULL,
+  `title` varchar(30) DEFAULT NULL,
+  `finished_condition` tinyint(1) DEFAULT NULL COMMENT '0-已修改完成完成；1-未修改完成',
+  `questions` varchar(100) DEFAULT NULL,
+  `start_time` date DEFAULT NULL,
+  `end_time` date DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '0' COMMENT '0-未发布；1-发布',
   `create_user` varchar(24) DEFAULT NULL,
   `create_time` date DEFAULT NULL,
   `update_user` varchar(24) DEFAULT NULL,
@@ -30,23 +62,17 @@ CREATE TABLE `class` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `class` */
+/*Data for the table `operation` */
 
-insert  into `class`(`id`,`course_id`,`name`,`create_user`,`create_time`,`update_user`,`update_time`,`delete_flag`) values ('testclass1','test1','19信计1',NULL,NULL,NULL,NULL,0),('testclass2','test1','19云计算',NULL,NULL,NULL,NULL,0);
+/*Table structure for table `operation_grade` */
 
-/*Table structure for table `course` */
+DROP TABLE IF EXISTS `operation_grade`;
 
-DROP TABLE IF EXISTS `course`;
-
-CREATE TABLE `course` (
+CREATE TABLE `operation_grade` (
   `id` varchar(24) NOT NULL,
-  `teacher_id` varchar(24) DEFAULT NULL,
-  `course_name` varchar(50) NOT NULL,
-  `course_info` varchar(200) DEFAULT NULL,
-  `pre_course` varchar(200) DEFAULT NULL,
-  `course_plan` varchar(200) DEFAULT NULL,
-  `bibliography` varchar(200) DEFAULT NULL COMMENT '参考书目',
-  `courseware` varchar(24) DEFAULT NULL COMMENT '课件地址id',
+  `operation_id` varchar(24) NOT NULL,
+  `student_id` varchar(24) NOT NULL,
+  `grade` int(11) DEFAULT NULL,
   `create_user` varchar(24) DEFAULT NULL,
   `create_time` date DEFAULT NULL,
   `update_user` varchar(24) DEFAULT NULL,
@@ -55,53 +81,7 @@ CREATE TABLE `course` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `course` */
-
-insert  into `course`(`id`,`teacher_id`,`course_name`,`course_info`,`pre_course`,`course_plan`,`bibliography`,`courseware`,`create_user`,`create_time`,`update_user`,`update_time`,`delete_flag`) values ('test1','test','C++语言',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0),('test2','test','Java语言',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0),('test3','test','数据结构与算法',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
-
-/*Table structure for table `student` */
-
-DROP TABLE IF EXISTS `student`;
-
-CREATE TABLE `student` (
-  `id` varchar(24) NOT NULL,
-  `class_id` varchar(24) DEFAULT NULL,
-  `name` varchar(30) DEFAULT NULL,
-  `email` varchar(30) DEFAULT NULL,
-  `password` varchar(30) DEFAULT NULL,
-  `student_code` varchar(24) DEFAULT NULL,
-  `info` varchar(100) DEFAULT NULL,
-  `create_user` varchar(24) DEFAULT NULL,
-  `create_time` date DEFAULT NULL,
-  `update_user` varchar(24) DEFAULT NULL,
-  `update_time` date DEFAULT NULL,
-  `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `student` */
-
-/*Table structure for table `teacher` */
-
-DROP TABLE IF EXISTS `teacher`;
-
-CREATE TABLE `teacher` (
-  `id` varchar(24) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `info` varchar(30) DEFAULT NULL,
-  `create_user` varchar(24) DEFAULT NULL,
-  `create_time` date DEFAULT NULL,
-  `update_user` varchar(24) DEFAULT NULL,
-  `update_time` date DEFAULT NULL,
-  `delete_flag` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `teacher` */
-
-insert  into `teacher`(`id`,`username`,`email`,`password`,`info`,`create_user`,`create_time`,`update_user`,`update_time`,`delete_flag`) values ('test','丰杰','18866856479@126.com','EFG@AB',NULL,NULL,NULL,NULL,NULL,NULL);
+/*Data for the table `operation_grade` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
